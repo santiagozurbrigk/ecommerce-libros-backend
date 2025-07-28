40a09646ed060f32afac47761bb6cc2d8dcc9e96
santiagozurbrigk <santiagozurbrigkwork@gmail.com>
2025-07-28 03:02:06 -0300

    Update CORS configuration

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: 'https://navajowhite-giraffe-485297.hostingersite.com',
-  credentials: true
+  origin: ['https://navajowhite-giraffe-485297.hostingersite.com', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 // Servir imágenes subidas
